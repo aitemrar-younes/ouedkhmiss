1ac73688743bddc2dd9c746db6906a174c187839
aitemrar-younes <aitemrar.younes@gmail.com>
2025-03-08 20:55:48 +0100

    added allowed_host

--- backend/root/settings.py
+++ backend/root/settings.py
@@ -33,7 +33,7 @@ SECRET_KEY = env("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = env.bool("DEBUG", default=False)
 
-ALLOWED_HOSTS = env.list("ALLOWED_HOSTS", default=["localhost",])
+ALLOWED_HOSTS = ['ouedkhmiss-app.onrender.com', 'localhost']
 
 # Application definition
 
